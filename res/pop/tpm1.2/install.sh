f2acb623ca074bf808b7fde1650d4b38dbafb40a
Wesley Helton <wesleyrobhelton@gmail.com>
2021-01-25 17:37:32 -0600

    Enable Systemd-boot signing.

--- res/pop/tpm1.2/install.sh
+++ res/pop/tpm1.2/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # Noah Bliss
 
+source /etc/mortar/mortar.env
+
+#If using xbootldr these paths may not be the same as /boot/efi.
+BOOTX64="/boot/efi/EFI/BOOT/BOOTX64.efi"
+#Path to systemd-boot bootloader efi image.
+SYSTEMD="/boot/efi/EFI/systemd/systemd-boot.efi"
+
 # Install the kernel upgrade hook for generation and signing of the efi.
 cp -r kernel /etc/
 
@@ -15,7 +22,17 @@ sed -i -e "/^TPMINDEX=.*/{s//TPMINDEX=$TPMINDEX/;:a" -e '$!N;$!b' -e '}' "$INITR
 sed -i -e "/^HEADERSHA256=.*/{s//HEADERSHA256=$HEADERSHA256/;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
 sed -i -e "/^HEADERFILE=.*/{s##HEADERFILE=\"$HEADERFILE\"#;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
 
+sed -i -e "/^BOOTX64=.*/{s##BOOTX64=\"$BOOTX64\"#;:a" -e '$!N;$!b' -e '}' "/etc/kernel/zzz-mortar-efigensign"
+sed -i -e "/^SYSTEMD=.*/{s##SYSTEMD=\"$SYSTEMD\"#;:a" -e '$!N;$!b' -e '}' "/etc/kernel/zzz-mortar-efigensign"
+
+
+
 update-initramfs -u
 
 echo "Initramfs updated. You need to run mortar-compilesigninstall [kernelpath] [initramfspath]"
 echo "Consult the README for more detail."
+
+sbsign --output $SYSTEMD --key $SECUREBOOT_DB_KEY --cert $SECUREBOOT_DB_CRT $SYSTEMD
+cp $SYSTEMD $BOOTX64
+
+echo "Systemd-boot signed for secureboot."
